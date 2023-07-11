5c238d729a25234a5d0760f05e10b06dd6f7c959
Manav Jethva <manavj764@gmail.com>
2023-07-11 22:12:19 +0530

    Update Registration

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import ladyDoctor from "../../assets/images/lady_doctor.png";
 import { useNavigate } from "react-router-dom";
@@ -6,17 +6,17 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Register() {
-  const [userType, setUserType] = useState("student");
+  // const [userType, setUserType] = useState("student");
   const [name, setName] = useState("");
   const [userId, setuserId] = useState("");
   const [password, setPassword] = useState("");
-  const[verified,setVerified]=useState(false);
-  const navigate=useNavigate();
-  const handleUserTypeChange = (userType) => {
-    setUserType(userType);
-    setuserId("");
-  };
+  const navigate = useNavigate();
 
+  // const handleUserTypeChange = (userType) => {
+  //   setUserType(userType);
+  //   setuserId("");
+  // };
+  const userType="student"
   const handleNameChange = (event) => {
     setName(event.target.value);
   };
@@ -32,24 +32,19 @@ function Register() {
   const handleLoginRedirect = () => {
     navigate('/login');
   };
+
   const validateRollNumber = () => {
-       const rollNumberRegex = /^[a-zA-Z]{3}_\d{7}$/;
-       return rollNumberRegex.test(userId);
-  } 
- 
-  
+    const rollNumberRegex = /^[a-zA-Z]{3}_\d{7}$/;
+    return rollNumberRegex.test(userId);
+  }
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    if(validateRollNumber()===false&&userType==='student'){
-    return toast.error('Please enter the roll number in the correct format.',{
-      position:"top-center"
-    });
-  }
-  if (userType === 'admin' && !userId.includes('@iiitm.ac.in')) {
-    return toast.error('Invalid email credentials.', {
-      position: 'top-center',
-    });
-  }
+    if (validateRollNumber() === false) {
+      return toast.error('Please enter the roll number in the correct format.', {
+        position: "top-center"
+      });
+    }
     console.log(`User Type: ${userType}`);
     console.log(`Name: ${name}`);
     console.log(`userId: ${userId}`);
@@ -63,13 +58,7 @@ function Register() {
     setuserId("");
     setPassword("");
 
-    let url = "";
-
-    if (userType === "student") {
-      url = 'http://localhost:3001/api/auth/register';
-    } else if (userType === "admin") {
-      url = "http://localhost:3001/api/auth/admin/register";
-    }
+    const url = 'http://localhost:3001/api/auth/register';
 
     axios
       .post(url, data)
@@ -80,30 +69,21 @@ function Register() {
         setuserId("");
         setPassword("");
         console.log(response.data)
-        if(response.data.verified===true){
-        setVerified(true); 
-        toast.success("Verification email sent", {
-          position: "top-center"
-        });
-      }
+        if (response.data.verified === true) {
+          toast.success("Verification email sent", {
+            position: "top-center"
+          });
+          navigate('/login');
+        }
       })
       .catch((error) => {
         console.error(error);
-        if (error.response.status === 409) 
-         toast.error("User Already exists",{
-          position:"top-center"
-         })
+        if (error.response.status === 409)
+          toast.error("User Already exists", {
+            position: "top-center"
+          })
       });
   };
-  useEffect(() => {
-    if (verified) {
-      const timer = setTimeout(() => {
-        navigate(`/login`);
-      }, 3000);
-  
-      return () => clearTimeout(timer);
-    }
-  }, [verified,navigate]);
 
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-purple-300 to-slate-300">
@@ -127,30 +107,6 @@ function Register() {
 
           <div className="flex justify-center pr-5">
             <form onSubmit={handleFormSubmit} className="max-w-md pl-20">
-              <div className="flex items-center justify-center my-3">
-                <div
-                  className={`mr-4 rounded-sm ${
-                    userType === "student"
-                      ? "bg-blue-500 text-white py-2 px-8 rounded"
-                      : "bg-gray-500 text-white py-2 px-8 rounded"
-                  }`}
-                  onClick={() => handleUserTypeChange("student")}
-                  style={{ cursor: "pointer" }}
-                >
-                  Student
-                </div>
-                <div
-                  className={`mr-4 rounded-sm ${
-                    userType === "admin"
-                      ? "bg-blue-500 text-white py-2 px-8 rounded"
-                      : "bg-gray-500 text-white py-2 px-8 rounded"
-                  }`}
-                  onClick={() => handleUserTypeChange("admin")}
-                  style={{ cursor: "pointer" }}
-                >
-                  Admin
-                </div>
-              </div>
               <label className="block mb-4">
                 Name:
                 <input
@@ -160,30 +116,16 @@ function Register() {
                   className="block w-full mt-1 pl-1"
                 />
               </label>
-              {userType === "student" ? (
-                <label className="block mb-4">
-                  Roll No:
-                  <input
-                    type="text"
-                    value={userId}
-                    onChange={handleuserIdChange}
-                    className="block w-full mt-1 pl-1"
-                    placeholder="bcs_2021XXX"
-                  />
-                </label>
-                
-              ) : (
-                <label className="block mb-4">
-                  Email:
-                  <input
-                    type="email"
-                    value={userId}
-                    onChange={handleuserIdChange}
-                    className="block w-full mt-1 pl-1"
-                  />
-                </label>
-              )}
-              
+              <label className="block mb-4">
+                Roll No:
+                <input
+                  type="text"
+                  value={userId}
+                  onChange={handleuserIdChange}
+                  className="block w-full mt-1 pl-1"
+                  placeholder="bcs_2021XXX"
+                />
+              </label>
               <label className="block mb-4">
                 Password:
                 <input
@@ -202,20 +144,19 @@ function Register() {
             </form>
           </div>
           <div className="flex justify-center mt-4">
-          <button
-            onClick={handleLoginRedirect}
-            className="text-black font-bold hover:underline mt-5"
-            style={{ marginLeft: '2.8rem' ,marginTop:"0.2rem"}}
-          >
-            Already registered? Login 
-          </button>
-        </div>
+            <button
+              onClick={handleLoginRedirect}
+              className="text-black font-bold hover:underline mt-5"
+              style={{ marginLeft: '2.8rem', marginTop: "0.2rem" }}
+            >
+              Already registered? Login
+            </button>
+          </div>
         </div>
       </div>
-      <ToastContainer/>
+      <ToastContainer />
     </div>
   );
 }
 
 export default Register;
-
